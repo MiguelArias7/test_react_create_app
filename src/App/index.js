3a569222796eaad7d16f7a8e43a931d10d2402cb
MiguelArias <miguelariasgiraldo2000@gmail.com>
2023-03-14 18:03:27 -0500

    Removed unnecessary comments

--- src/App/index.js
+++ src/App/index.js
@@ -1,16 +1,6 @@
-// import logo from './logo.svg';
-// import "./App.css";
-
 import React from "react";
 import { AppUI } from "./AppUI";
 import { TodoProvider } from "../TodoContext";
-
-// const defaultTodos = [
-//   { id: "1", text: "Terminar A", completed: false },
-//   { id: "2", text: "Definir B", completed: true },
-//   { id: "3", text: "Completar C", completed: false },
-//   { id: "4", text: "Totima", completed: true },
-// ];
 
 function App() {
   return (
