bc02a353f77e859414d5f80577bee9107e921f69
MiguelArias7 <miguel.ariasg@autonoma.edu.co>
2023-04-05 16:23:11 -0500

    Style: Se ajustan los estilos para el componente de alerta para el cambio en storage.

--- src/StorageChangeAlert/index.js
+++ src/StorageChangeAlert/index.js
@@ -1,13 +1,20 @@
 import React from "react";
 import { withStorageListener } from "./withStorageListener";
+import "./StorageChangeAlert.css";
 
 function StorageChangeAlert({ show, toggleShow }) {
-  return (
-    <React.Fragment>
-      {show && <p>¿Hubo cambios?</p>}
-      {show && <button onClick={() => toggleShow()}> ¿Recargar ToDos?</button>}
-    </React.Fragment>
-  );
+  if (show) {
+    return (
+      <div className="StorageChangeAlert-bg">
+        <div className="StorageChangeAlert">
+          <p>Hubo cambios en otra página.</p>
+          <p>Presione el botón para recargar los ToDos.</p>
+          <button onClick={() => toggleShow()}> ¿Recargar ToDos?</button>
+        </div>
+      </div>
+    );
+  }
+  return <React.Fragment></React.Fragment>;
 }
 const StoreChangeAlertWithStorageListener =
   withStorageListener(StorageChangeAlert);
