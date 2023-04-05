13169effaede8abb68d9caa6ca5e5f10aa985378
MiguelArias7 <miguel.ariasg@autonoma.edu.co>
2023-04-05 14:03:00 -0500

    Solo se hace render una vez

--- src/App/useLocalStorage.js
+++ src/App/useLocalStorage.js
@@ -24,7 +24,7 @@ function useLocalStorage(itemName, initialValue) {
         setError(error);
       }
     }, 1500);
-  });
+  }, []);
 
   const saveItem = (newTodos) => {
     // const stringifiedTodos = JSON.stringify(newTodos);
